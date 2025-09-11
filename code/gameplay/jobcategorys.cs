d12d2068c23c35c1db7be3f51f2d83e0dd395d33
iamekaj <realiamekaj@gmail.com>
2025-09-11 18:06:36 -0400

    -Added More UI Elements for Edit Door Groups

--- code/gameplay/jobcategorys.cs
+++ code/gameplay/jobcategorys.cs
@@ -3,14 +3,13 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 
-[GameResource("Job Category", "jobcategory", "Defines a DarkRP style job category")]
+[GameResource("JobCategory", "jobcateg", "Defines a DarkRP style job category")]
 public class JobCategoryResource : GameResource
 {
     [Property] public string Name { get; set; }
 
     [Property] public Color Color { get; set; } = Color.White; // optional color for UI
 
-    [Property, ResourceType("vmdl")] public string Icon { get; set; } // optional model/icon
 
     // Keep track of all categories
     public static IReadOnlyList<JobCategoryResource> All => _all;
