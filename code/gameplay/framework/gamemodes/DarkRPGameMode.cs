2d9bbe371c0fc07f494782851c23fbe02a6cbd4a
iamekaj <realiamekaj@gmail.com>
2025-12-24 19:50:03 -0500

    -Created Jail Timer UI/Created Extra Unarrested Logic
    I got the Jail timer to show up/hide with the total time and it even ticks down!
    
    I also wanted to have the Unarrest System to properly check for Sweps that are 'Starting Sweps' or 'Legal Sweps'. If so, don't remove the weapon.
    
    I also added another setting to the DarkRP Gamemode to allow the Owners to Allow/Disallow Illegal Sweps  being taken away when unarrested. Inorder to do this I had to go in and change some of the Vars in the DarkRP Gamemode to be lists instead of arrays. I might change this back if there is any problems.

--- code/gameplay/framework/gamemodes/DarkRPGameMode.cs
+++ code/gameplay/framework/gamemodes/DarkRPGameMode.cs
@@ -6,24 +6,25 @@ public partial class DarkRPGamemode : GameModeBase
 {
 	//DarkRP Basic Info
 	// Maybe put this on the Respawn Component? [Property, Feature("DarkRP Gamemode Info")] public float SpawnProtection { get; set; }
-	[Property, Feature("Basic Info")] public bool DisableDefualtF2MenuUI { get; set; }
-	[Property, Feature("Basic Info")] public bool DisableDefualtF4MenuUI { get; set; }	
-	[Property, Feature("Basic Info")] public bool DisableDefualtScoreboardUI { get; set; }	
-	[Property, Feature("Basic Info")] public bool DisableDefualtModerationSystem { get; set; }	
-	[Property, Feature("Basic Info")] public bool DisableDefualtSpawnProtectionSystem { get; set; }	
+	[Property, Feature("Basic Info")] public bool DisableDefaultF2MenuUI { get; set; }
+	[Property, Feature("Basic Info")] public bool DisableDefaultF4MenuUI { get; set; }	
+	[Property, Feature("Basic Info")] public bool DisableDefaultScoreboardUI { get; set; }	
+	[Property, Feature("Basic Info")] public bool DisableDefaultModerationSystem { get; set; }	
+	[Property, Feature("Basic Info")] public bool DisableDefaultSpawnProtectionSystem { get; set; }	
 	[Property, Feature("Basic Info")] public int SalaryPaymentSystemCycleTime { get; set; }
 	[Property, Feature("Basic Info")] public int SalaryPaymentMultiplier { get; set; }
 	[Property, Feature("Basic Info")] public int EntityPriceMultiplier { get; set; }
-	[Property, Feature("Basic Info")] public PickupableEntityBase[] StartingEquipment { get; set; }
+	[Property, Feature("Basic Info")] public List<SwepLists> StartingEquipment { get; set; }
 	[Property, Feature("Basic Info")] public PrefabFile MoneyEntity { get; set; }
 	[Property, Feature("Basic Info")] public int MaxPropSpawnedAmount { get; set; }
 	[Property, Feature("Basic Info"), Description("If True a Job has ")] public int TempPlayTimeTotalCycleAmount { get; set; }
 
 	//DarkRP Law System Info
-	[Property, Feature("Law Info")] public GameObject[] JailPos { get; set; }
+	[Property, Feature("Law Info")] public List<GameObject> JailPos { get; set; }
 	[Property, Feature("Law Info")] public int JailTime { get; set; }
 	[Property, Feature("Law Info")] public Model LawBoardModel { get; set; }
 	[Property, Feature("Law Info")] public int MaxLawsOnBoard { get; set; }
+	[Property, Feature("Law Info"), Description("Should Players when they are unarrested get their Illegal Sweps back? True=Yes False=No")] public bool GiveIllegalSwepsBackOnUnarrest { get; set; }
 	[Property, Feature("Law Info"), Title("Lottery System")] public bool ToggleLottery { get; set; }
 	[Property, Feature("Law Info")] public int MinLotteryPrice { get; set; }
 	[Property, Feature("Law Info")] public int MaxLotteryPrice { get; set; }
@@ -32,14 +33,14 @@ public partial class DarkRPGamemode : GameModeBase
 	[Property, Feature("Law Info")] public SoundEvent AlarmSound { get; set; }
 	[Property, Feature("Law Info")] public SoundEvent ArrestedSFX { get; set; }
 	[Property, Feature("Law Info")] public SoundEvent UnArrestedSFX { get; set; }
-	[Property, Feature("Law Info")] public PickupableEntityBase[] PDStartingEquipment { get; set; }
-	[Property, Feature("Law Info")] public PickupableEntityBase[] LicensedEquipment { get; set; }
+	[Property, Feature("Law Info")] public List<PickupableEntity> PDStartingEquipment { get; set; }
+	[Property, Feature("Law Info")] public List<PickupableEntity> LicensedEquipment { get; set; }
 
 	
 	//DarkRP Vote System Info
-	[Property, Feature("Vote Info")] public int DefualtVoteDuration { get; set; }
-	[Property, Feature("Vote Info")] public int DefualtMinimumVoteRequired { get; set; }
-	[Property, Feature("Vote Info")] public float DefualtRequiredYesPercentage { get; set; }
+	[Property, Feature("Vote Info")] public int DefaultVoteDuration { get; set; }
+	[Property, Feature("Vote Info")] public int DefaultMinimumVoteRequired { get; set; }
+	[Property, Feature("Vote Info")] public float DefaultRequiredYesPercentage { get; set; }
 	[Property, Feature("Vote Info")] public SoundEvent VoteStartSFX { get; set; }
 
 
@@ -61,7 +62,12 @@ public partial class DarkRPGamemode : GameModeBase
 
 
 
+ 	public struct SwepLists
+	{
+	[KeyProperty] public List<PickupableEntity> Swep { get; set; }
+	public int SwepPosIndex { get; set; }
 
+	}
 
 
 
