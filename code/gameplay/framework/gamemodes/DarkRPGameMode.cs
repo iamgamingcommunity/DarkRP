3e489909bbf22ad6e5c04c2b2cb721d33d8b0858
iamekaj <realiamekaj@gmail.com>
2026-01-14 20:15:13 -0500

    -Further Tried to Figure Out F4 Menu Job
    -Created 5 Jobs 'Hobo, Thief, Mob Boss, Civil Protection, Gun Dealer'
    -Fixed/Finished Job Info 'Mayor, Teaguy, Citizen'
    -Tried to get the Job Category UI To align and flow right(This is a pain, and I'm coming close to figuring it out)
    -Got Some Logic done for the 'Become Job'(Still buggy but very close on working)
    -Added a New Var to DarkRP Gamemode
    -

--- code/gameplay/framework/gamemodes/DarkRPGameMode.cs
+++ code/gameplay/framework/gamemodes/DarkRPGameMode.cs
@@ -12,6 +12,7 @@ public partial class DarkRPGamemode : GameModeBase
 	[Property, Feature("Basic Info")] public bool DisableDefaultScoreboardUI { get; set; }	
 	[Property, Feature("Basic Info")] public bool DisableDefaultModerationSystem { get; set; }	
 	[Property, Feature("Basic Info")] public bool DisableDefaultSpawnProtectionSystem { get; set; }	
+	[Property, Feature("Basic Info")] public bool DisableRespawningAfterBecomingJob { get; set; }	
 	[Property, Feature("Basic Info")] public int SalaryPaymentSystemCycleTime { get; set; }
 	[Property, Feature("Basic Info")] public int SalaryPaymentMultiplier { get; set; }
 	[Property, Feature("Basic Info")] public int EntityPriceMultiplier { get; set; }
