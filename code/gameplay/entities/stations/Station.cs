cf36d6b07d9184e55f92ff4156762b02d61a282c
iamekaj <realiamekaj@gmail.com>
2026-05-27 01:38:21 -0400

    -Continued to work on Stations
    Some bugs fixed and made it function more like the originals HL charging stations.

--- code/gameplay/entities/stations/Station.cs
+++ code/gameplay/entities/stations/Station.cs
@@ -4,26 +4,38 @@ using Sandbox;
 [Description("A Station is a thing where a person can go to receive a 'Resource'. This 'Resource' can be Health, Armor, etc.")]
 public sealed class Station : BaseEntity
 {
-	
-	[Property, Group("Station"), Feature("Station")] public SoundEvent StationSFX {get; set;}
-	[Property, Group("Station"), Feature("Station")] public SoundEvent StationNoResourceLeftSFX {get; set;}
-	[Property, Group("Station"), Feature("Station")] public SoundEvent StationDeniedSFX {get; set;}
-	[Property, Group("Station"), Feature("Station")] public SoundEvent StationStoredResourceReplienished {get; set;}
-	[Property, Group("Station"), Feature("Station")] public float StationReplienishDelay {get; set;}
-	[Property, Group("Station"), Feature("Station")] public float StationInteractionDelay {get; set;}
-	[Property, Group("Station"), Feature("Station")] public int StationResourceAmountGiven {get; set;}
-	[Property, Group("Station"), Feature("Station")] public int StationResourceGivenDelay {get; set;}
-	[Property, Group("Station"), Feature("Station")] public int StationStoredResourceAmount {get; set;}
-	[Property, Group("Station"), Feature("Station")] public int StationStoredMaxResourceAmount {get; set;}
-	[Property, Group("Station"), Feature("Station")] public bool IsStationGivingResource {get; set;}
-	[Property, Group("Station"), Feature("Station"), Description("Every x amount(StationReplienishDelay) should we refill the current 'Resource' Amount to the 'StationStoredMaxResourceAmount'?")] public bool IsReplenishAlways {get; set;}
-	[Property, Group("Station"), Feature("Station")] public SoundHandle StationSoundHandle {get; set;}
+	//Station Variables 
+	[Property, Group("Station SFX"), Feature("Station")] public SoundEvent StationSFX {get; set;}
+	[Property, Group("Station SFX"), Feature("Station")] public SoundEvent StationNoResourceLeftSFX {get; set;}
+	[Property, Group("Station SFX"), Feature("Station")] public SoundEvent StationDeniedSFX {get; set;}
+	[Property, Group("Station SFX"), Feature("Station")] public SoundEvent StationStoredResourceReplienished {get; set;}
+	[Property, Group("Station Basic Info"), Feature("Station")] public float StationReplienishDelay {get; set;}
+	[Property, Group("Station Basic Info"), Feature("Station")] public float StationInteractionDelay {get; set;}
+	[Property, Group("Station Basic Info"), Feature("Station")] public int StationResourceAmountGiven {get; set;}
+	[Property, Group("Station Basic Info"), Feature("Station")] public int StationResourceGivenDelay {get; set;}
+	[Property, Group("Station Basic Info"), Feature("Station")] public int StationStoredResourceAmount {get; set;}
+	[Property, Group("Station Basic Info"), Feature("Station")] public int StationStoredMaxResourceAmount {get; set;}
+	[Property, Group("Station Basic Info"), Feature("Station")] public bool IsStationResourceInfinite {get; set;}
+	[Property, Group("Station Basic Info"), Feature("Station")] public bool IsStationGivingResource {get; set;}
+	[Property, Group("Station Basic Info"), Feature("Station"), Description("Every x amount(StationReplienishDelay) should we refill the current 'Resource' Amount to the 'StationStoredMaxResourceAmount'?")] public bool IsReplenishAlways {get; set;}
+	[Property, Group("Station SFX"), Feature("Station")] public SoundHandle StationSoundHandle {get; set;}
 
 	[Property, Group("Station"), Feature("Station"), Description("Will this Station Add or Subtract from the Player 'Resource'?")] public StationAction StationActionType {get; set;}
 	[Property, Group("Station"), Feature("Station"), Description("What type of 'Resource' will this Station Add or Remove from the Player when they interact with the station?")] public StationResource StationResourceType {get; set;}
 
 
+	public void RefireStationLogic(PlayerController LastInteractingPlayer )
+		{
+			this.Interacted?.Invoke(LastInteractingPlayer);
+		}
 
+
+
+
+
+
+
+	//Station Enums
 	public enum StationAction
 	{
 		Add,
